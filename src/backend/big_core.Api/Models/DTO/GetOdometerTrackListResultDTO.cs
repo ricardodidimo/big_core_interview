6dda66ca579907041140feb1feff21b3142f1e02
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-26 00:02:21 -0300

    Feat: Added 'GetOdometerTrackListResultDTO' as a type definition for the api expected return

---
+++ src/backend/big_core.Api/Models/DTO/GetOdometerTrackListResultDTO.cs
@@ -0,0 +1,44 @@
+namespace big_core.Api.Models.DTO;
+
+public record GetOdometerTrackListResultDTO(
+    List<OdometerData> Data,
+    int TotalItems,
+    int NumberOfRowPage,
+    int PageActive,
+    int TotalPages
+);
+
+public record OdometerData(
+    int VehicleId,
+    string VehicleIdTms,
+    int DivisionId,
+    string DivisionName,
+    int OperationId,
+    string OperationName,
+    string Connector,
+    string LicensePlate,
+    int DriverId,
+    string DriverName,
+    string DriverIdTms,
+    int DriverDivisionId,
+    string DriverDivisionName,
+    int DriverOperationId,
+    string DriverOperationName,
+    DateTime DateProcess,
+    int Interval,
+    int Timezone,
+    bool Moving,
+    string Id,
+    bool Delayed,
+    int Odometer,
+    int OdometerKm,
+    bool Ignition,
+    string IgnitionStatus,
+    int Bearing,
+    Point Point,
+    int Speed,
+    int Quality,
+    DateTime Date
+);
+
+public record Point(double Latitude, double Longitude);
