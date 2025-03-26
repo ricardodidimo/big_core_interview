cc8ab13ce78c1e0840b3e0f4bfa78132f20c1bb7
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-26 00:31:45 -0300

    Fix: removed nullable operator for props on GetOdemeterTrackerListFilterDTO

--- src/backend/big_core.Api/Models/DTO/GetOdemeterTrackerListFilterDTO.cs
+++ src/backend/big_core.Api/Models/DTO/GetOdemeterTrackerListFilterDTO.cs
@@ -3,9 +3,9 @@ namespace big_core.Api.Models.DTO;
 public record GetOdemeterTrackerListFilterDTO(
     DateTime StartDate,
     DateTime EndDate,
-    string[]? IdTms,
-    string[]? LicensePlates,
-    string[]? DivisionIds,
+    string[] IdTms,
+    string[] LicensePlates,
+    string[] DivisionIds,
     int Rows,
     int Page
 );
