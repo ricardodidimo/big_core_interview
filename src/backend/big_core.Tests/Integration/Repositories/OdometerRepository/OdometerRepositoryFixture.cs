2f08cae8cb182e759d3773d7d73df05f830dd10f
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-26 15:38:09 -0300

    Refactor: centralized http factory instance creation on an extension method to avoid code duplicates

--- src/backend/big_core.Tests/Integration/Repositories/OdometerRepository/OdometerRepositoryFixture.cs
+++ src/backend/big_core.Tests/Integration/Repositories/OdometerRepository/OdometerRepositoryFixture.cs
@@ -2,7 +2,7 @@ namespace big_core.Tests.Integration.Repositories.OdometerRepository;
 
 using Microsoft.Extensions.DependencyInjection;
 using big_core.Api.Repository.Odometer;
-using big_core.Common;
+using big_core.Api.Helpers;
 
 [CollectionDefinition("OdometerRepository Collection")]
 public class OdometerRepositoryCollection : ICollectionFixture<OdometerRepositoryFixture> { }
@@ -13,19 +13,8 @@ public class OdometerRepositoryFixture
 
     public OdometerRepositoryFixture()
     {
-        var baseUrl = Environment.GetEnvironmentVariable(CommonConstants.ApiBaseUrlKey) 
-        ?? throw new Exception($"{CommonConstants.ApiBaseUrlKey} is missing");
-        var authHeaderToken = Environment.GetEnvironmentVariable(CommonConstants.ApiAuthHeaderTokenKey) 
-        ?? throw new Exception($"{CommonConstants.ApiAuthHeaderTokenKey} is missing");
-
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddHttpClient<IOdometerRepository, OdometerHttpRepository>(client =>
-        {
-            client.BaseAddress = new Uri(baseUrl);
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
-                "Basic", authHeaderToken
-            );
-        });
+        serviceCollection.RegisterHttpFactoryInstance();
 
         var serviceProvider = serviceCollection.BuildServiceProvider();
         OdometerRepository = serviceProvider.GetRequiredService<IOdometerRepository>();
