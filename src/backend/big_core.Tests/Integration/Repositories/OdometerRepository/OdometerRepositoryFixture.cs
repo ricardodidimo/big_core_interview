be377cf80135474efe5513f2d883a0540a6e9da5
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-27 12:23:45 -0300

    Feat: Added cache logic to 'OdometerHttpRepository'; Register FAKE cache impl to test cases for such repository

--- src/backend/big_core.Tests/Integration/Repositories/OdometerRepository/OdometerRepositoryFixture.cs
+++ src/backend/big_core.Tests/Integration/Repositories/OdometerRepository/OdometerRepositoryFixture.cs
@@ -3,6 +3,7 @@ namespace big_core.Tests.Integration.Repositories.OdometerRepository;
 using Microsoft.Extensions.DependencyInjection;
 using big_core.Api.Repository.Odometer;
 using big_core.Api.Helpers;
+using big_core.Api.Services.Cache;
 
 [CollectionDefinition("OdometerRepository Collection")]
 public class OdometerRepositoryCollection : ICollectionFixture<OdometerRepositoryFixture> { }
@@ -15,7 +16,7 @@ public class OdometerRepositoryFixture
     {
         var serviceCollection = new ServiceCollection();
         serviceCollection.RegisterHttpFactoryInstance();
-
+        serviceCollection.AddScoped<ICacheService, FakeCacheService>();
         var serviceProvider = serviceCollection.BuildServiceProvider();
         OdometerRepository = serviceProvider.GetRequiredService<IOdometerRepository>();
     }
