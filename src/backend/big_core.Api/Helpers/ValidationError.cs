e7d67dfe9c7c184ac71059ba0b4edeefe65a9e2f
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-27 01:02:36 -0300

    Refactor: removed special strings from ValidationError class. Re-Added them as static consts

--- src/backend/big_core.Api/Helpers/ValidationError.cs
+++ src/backend/big_core.Api/Helpers/ValidationError.cs
@@ -6,12 +6,14 @@ public class ValidationError : IError
     public List<IError> Reasons => new();
     public string Message { get; }
     public Dictionary<string, object> Metadata { get; } = new();
+    public static readonly string PROPERTY_NAME_METADATA_KEY = "PropertyName";
+    public static readonly string MESSAGES_METADATA_KEY = "Messages";
 
     public ValidationError(string propertyName, IEnumerable<string> messages)
     {
         Message = $"Validation failed for property '{propertyName}'";
-        Metadata["PropertyName"] = propertyName;
-        Metadata["Messages"] = messages.ToArray();
+        Metadata[PROPERTY_NAME_METADATA_KEY] = propertyName;
+        Metadata[MESSAGES_METADATA_KEY] = messages.ToArray();
     }
 
     static public List<ValidationError> CreateErrorsList(List<FluentValidation.Results.ValidationFailure> validationFailures)
