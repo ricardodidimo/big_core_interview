dd306157523dce5e82694f790d42a521ec9736a0
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-26 23:39:10 -0300

    Feat: Added NumberHandling configuration to 'JsonSettings' object for more deserialization safety

--- src/backend/big_core.Api/Helpers/JsonSettings.cs
+++ src/backend/big_core.Api/Helpers/JsonSettings.cs
@@ -1,12 +1,14 @@
 namespace big_core.Api.Helpers;
 
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public static class JsonSettings
 {
     public static readonly JsonSerializerOptions DefaultOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals,
         WriteIndented = true
     };
 }
