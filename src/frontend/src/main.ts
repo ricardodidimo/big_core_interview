9022d1b03d5f6fc9b9be9a56088d87f3f7be046d
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-03-28 22:49:22 -0300

    Chore: added fontawesome and vuetify config to 'main.ts'

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,9 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "@fortawesome/fontawesome-free/css/all.css";
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-import 'vuetify/styles/main.css';
-import { createVuetify } from 'vuetify';
-const vuetify = createVuetify();
+import { createVuetify } from "vuetify";
+import "vuetify/styles/main.css";
+import "vuetify/dist/vuetify.css";
+import { fa } from "vuetify/iconsets/fa";
+import i18n from "./i18n";
 
-createApp(App).use(vuetify).mount('#app')
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "fa",
+    sets: { fa },
+  },
+});
+
+createApp(App).use(i18n).use(vuetify).mount("#app");
